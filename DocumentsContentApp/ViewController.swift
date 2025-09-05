aab88cbd5b4df572cc0f83480a6e896f2c0fd42c
Nikita <nktlut@yandex.ru>
2025-09-05 14:46:33 +0800

    Initial Commit

---
+++ DocumentsContentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DocumentsContentApp
+//
+//  Created by Nikita on 05.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
